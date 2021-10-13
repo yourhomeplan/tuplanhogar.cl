ad853af47f3a319ac227a91ee1c3ff5ee304e627
Alexander Salas Bastidas <ajsb85@firechip.dev>
2021-10-13 04:51:09 +0200

    fix: package ID

--- app/src/androidTest/java/cl/tuplanhogar/catalogo/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/cl/tuplanhogar/catalogo/ExampleInstrumentedTest.java
@@ -35,6 +35,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("ms.appcenter.sampleapp.android", appContext.getPackageName());
+        assertEquals("cl.tuplanhogar.catalogo", appContext.getPackageName());
     }
 }
